ff30584aabd5c0f3539983014e5002c321908139
lorenzo-damico <loredam@hotmail.it>
2020-08-04 15:59:03 +0200

    Aggiunta stampa del prezzo per tutte le categorie

--- js/script.js
+++ js/script.js
@@ -6,5 +6,27 @@ var distanza = parseInt(prompt("Inserire il numero di chilometri che verranno pe
 
 // 3. Definisco la tariffa base del viaggio
 var tariffaBase = distanza * 0.21;
+
+// 4. Definisco gli sconti per i minorenni e per gli over 65
+var scontoMinorenni = (tariffaBase / 100) * 20;
+var scontoOver65 = (tariffaBase / 100) * 40;
+
+// 5. Definisco le tariffe scontate per i minorenni e per gli over 65
+var tariffaMinorenni = tariffaBase - scontoMinorenni;
+var tariffaOver65 = tariffaBase - scontoOver65;
+
+// 6. Stampo il risultato in base all'età inserita
+if (eta < 18) {
+  document.getElementById('costo').innerHTML = tariffaMinorenni + " &euro;";
+} else if (eta >= 65) {
+  document.getElementById('costo').innerHTML = tariffaOver65 + " &euro;";
+} else {
+  document.getElementById('costo').innerHTML = tariffaBase + " &euro;";
+}
+
 // Debug
 console.log(tariffaBase);
+console.log(scontoMinorenni);
+console.log(tariffaMinorenni);
+console.log(scontoOver65);
+console.log(tariffaOver65);
